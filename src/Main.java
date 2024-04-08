3024f636bf9b84fe33c26cd30b2aa699374401ae
dollemika <dollemika@mail.ru>
2024-04-08 16:42:10 +0300

    функции

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> array = new ArrayList<>();
-        System.out.println("вводите целые положительные числа");
-        Scanner read = new Scanner(System.in);
-        array.add(read.nextInt());
-        while (array.getLast()>0){
-            array.add(read.nextInt());
-        }
-        array.removeLast();
+        readListOfPositiveInt(array);
         System.out.println(array);
         array.sort(Comparator.naturalOrder());
         System.out.println(array);
+        double m;
+        m = getM(array);
+        System.out.println(m);
+    }
+
+    private static void readListOfPositiveInt(ArrayList<Integer> array) {
+        System.out.println("вводите целые положительные числа");
+        Scanner read = new Scanner(System.in);
+        int n;
+        n = read.nextInt();
+        while (n>0){
+            array.add(n);
+            n = read.nextInt();
+        }
+    }
+
+    private static double getM(ArrayList<Integer> array) {
         double m;
         if (array.size()%2 == 0)
             m = (double)(array.get(array.size()/2-1) + array.get(array.size()/2))/2;
         else
             m = array.get(array.size()/2);
-        System.out.println(m);
+        return m;
     }
 }
